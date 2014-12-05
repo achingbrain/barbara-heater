fd80aa3e1340d96e2638cde200ff7d011d44d78e
achingbrain <alex@achingbrain.net>
2014-12-05 14:30:01 +0000

    sensible defaults

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ if(!process.env.BARBARA_BREW) {
 
 process.env.BARBARA_PORT = process.env.BARBARA_PORT || 7582
 process.env.BARBARA_TEMPERATURE_SENSOR = process.env.BARBARA_TEMPERATURE_SENSOR || 'http://localhost:7583'
-process.env.BARBARA_DATABASE = process.env.BARBARA_DATABASE || 'http://localhost:5984'
+process.env.BARBARA_DATABASE = process.env.BARBARA_DATABASE || 'http://silenus.local:5984'
 process.env.BARBARA_NOTIFICATION_INTERVAL = process.env.BARBARA_NOTIFICATION_INTERVAL || 10000
 process.env.BARBARA_MAX_TEMPERATURE = parseInt(process.env.BARBARA_MAX_TEMPERATURE || 30, 10)
 process.env.BARBARA_MIN_TEMPERATURE = parseInt(process.env.BARBARA_MIN_TEMPERATURE || 20, 10)
@@ -21,7 +21,9 @@ var container = new Container()
 // set up logging
 container.createAndRegister('logger', winston.Logger, {
   transports: [
-    new winston.transports.Console()
+    new winston.transports.Console({
+      colorize: true
+    })
   ]
 })
 
@@ -45,7 +47,8 @@ check.on('ready', function() {
     resourceDirectory:'./lib/resources',
     resourceCreator: function(resource, name) {
       return container.createAndRegister(name + 'Resource', resource)
-    }
+    },
+    logger: container.find('logger')
   })
 
   var server = Hapi.createServer('0.0.0.0', process.env.BARBARA_PORT, {
@@ -53,6 +56,6 @@ check.on('ready', function() {
   })
   server.addRoutes(columbo.discover())
   server.start(function() {
-    LOG.info('RESTServer', 'Running at', 'http://localhost:' + process.env.BARBARA_PORT)
+    container.find('logger').info('RESTServer', 'Running at', 'http://localhost:' + process.env.BARBARA_PORT)
   })
 })
